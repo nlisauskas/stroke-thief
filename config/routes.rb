08956d9e2096cf5091e706f842a16ac61fd9658d
nlisauskas <nlisausk@alumni.nd.edu>
2019-09-18 19:18:43 -0400

    tried to get back to square one working on local machine

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,6 @@ Rails.application.routes.draw do
     end
     resources :rounds
     resources :courses
-      get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-        !request.xhr? && request.format.html?
-      end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
